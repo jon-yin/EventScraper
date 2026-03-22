161038599694bd4dd720be3f12ec90ca6de63e1f
jon-yin <yin.jonathan.2@gmail.com>
2026-03-22 01:25:57 -0400

    update meetup scraper with data models

--- scrapers/meetup.go
+++ scrapers/meetup.go
@@ -26,9 +26,10 @@ type Meetup struct {
 }
 
 type MeetupQueryVars struct {
-	First int     `json:"first"`
-	Lat   float64 `json:"lat"`
-	Lon   float64 `json:"lon"`
+	First             int     `json:"first"`
+	Lat               float64 `json:"lat"`
+	Lon               float64 `json:"lon"`
+	DataConfiguration string  `json:"dataConfiguration"`
 }
 
 type PersistedQuery struct {
@@ -40,10 +41,72 @@ type MeetupExtensions struct {
 	PersistedQuery PersistedQuery `json:"persistedQuery"`
 }
 
+type OperationName string
+
+const (
+	RecommendedEventsWithSeries OperationName = "recommendedEventsWithSeries"
+)
+
 type MeetupOperation struct {
-	OperationName string           `json:"operationName"`
+	OperationName OperationName    `json:"operationName"`
 	Variables     MeetupQueryVars  `json:"variables"`
 	Extensions    MeetupExtensions `json:"extensions"`
+}
+
+type RecommendedEventsResponse struct {
+	Data DataPayload `json:"data"`
+}
+
+type DataPayload struct {
+	Result Result `json:"result"`
+}
+
+type Result struct {
+	TotalCount int      `json:"totalCount"`
+	Events     []Edge   `json:"edges"`
+	PageInfo   PageInfo `json:"pageInfo"`
+}
+
+type PageInfo struct {
+	HasNextPage bool   `json:"hasNextPage"`
+	EndCursor   string `json:"endCursor"`
+}
+
+type Edge struct {
+	Node Event `json:"node"`
+}
+
+// EventType either describes an ONLINE or PHYSICAL event
+type EventType string
+
+const (
+	Online   EventType = "ONLINE"
+	Physical EventType = "PHYSICAL"
+)
+
+type Event struct {
+	Title       string    `json:"title"`
+	DateTime    time.Time `json:"dateTime"`
+	Description string    `json:"description"`
+	EventType   EventType `json:"eventType"`
+}
+
+func (e *EventType) MarshalJSON() ([]byte, error) {
+	return json.Marshal(string(*e))
+}
+
+func (e *EventType) UnmarshalJSON(bytes []byte) error {
+	var resString string
+	err := json.Unmarshal(bytes, &resString)
+	if err != nil {
+		return err
+	}
+	switch EventType(resString) {
+	case Online, Physical:
+		*e = EventType(resString)
+		return nil
+	}
+	return fmt.Errorf("got unknown event type: %s", resString)
 }
 
 const recommendedEventsHash = "3f7480361301be1b3208df0cd724930a22f7741d3c24666ab5b37a381ff4e0e8"
@@ -75,6 +138,7 @@ func (m *Meetup) getLatAndLon(ctx context.Context) (lat float64, lon float64, er
 	if err != nil {
 		return 0, 0, fmt.Errorf("error getting home bytes: %w", err)
 	}
+	// TODO: remove these test files
 	err = os.WriteFile("meetup_captured.html", bytes, 0666)
 	if err != nil {
 		return 0, 0, fmt.Errorf("error writing home file: %w", err)
@@ -95,9 +159,9 @@ func (m *Meetup) getLatAndLon(ctx context.Context) (lat float64, lon float64, er
 		if err != nil {
 			return 0, 0, fmt.Errorf("error parsing lon: %w", err)
 		}
-		m.log.Debug("found lon", slog.Float64("lon", lat))
+		m.log.Debug("found lon", slog.Float64("lon", lon))
 	} else {
-		return 0, 0, errors.New("latitude not found from meetup")
+		return 0, 0, errors.New("longitude not found from meetup")
 	}
 	return lat, lon, nil
 }
@@ -108,11 +172,12 @@ func (m *Meetup) Scrape(ctx context.Context) error {
 		return fmt.Errorf("error trying to fetch lat and lon from meetup: %w", err)
 	}
 	op := MeetupOperation{
-		OperationName: "recommendedEventsWithSeries",
+		OperationName: RecommendedEventsWithSeries,
 		Variables: MeetupQueryVars{
-			First: 12,
-			Lat:   lat,
-			Lon:   lon,
+			First:             12,
+			Lat:               lat,
+			Lon:               lon,
+			DataConfiguration: `{"isSimplifiedSearchEnabled":true}`,
 		},
 		Extensions: MeetupExtensions{
 			PersistedQuery: PersistedQuery{
@@ -135,12 +200,16 @@ func (m *Meetup) Scrape(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("error reading response: %w", err)
 	}
+	defer res.Body.Close()
 	m.log.Debug("Got response from gql", slog.Int("statusCode", res.StatusCode))
-	bytes, err := io.ReadAll(res.Body)
+	gqlData, err := io.ReadAll(res.Body)
 	if err != nil {
 		return fmt.Errorf("error getting gql bytes: %w", err)
 	}
-	err = os.WriteFile("meetup_gql.json", bytes, 0666)
-	defer res.Body.Close()
+	// TODO: remove these test files
+	err = os.WriteFile("meetup_gql.json", gqlData, 0666)
+	if err != nil {
+		return fmt.Errorf("error writing gql bytes: %w", err)
+	}
 	return nil
 }
